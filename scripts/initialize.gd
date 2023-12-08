9074c4c10473423d298f0dd1c8f176fc2d4bfee5
cbbor <c.b.borisova@gmail.com>
2023-12-07 18:12:19 -0600

    terrain attempt

--- scripts/initialize.gd
+++ scripts/initialize.gd
@@ -1,6 +1,9 @@
 extends Node3D
 
+const HTerrainData = preload("res://addons/zylann.hterrain/hterrain_data.gd")
+@onready var _terrain = %Terrain
 var xr_interface: XRInterface
+
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -14,3 +17,22 @@ func _ready():
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized. Please check if your headset is connected.")
+
+func _on_button_pressed(name):
+	if (name == 'trigger_click'):
+		# Get the image
+		var data : HTerrainData = _terrain.get_data()
+		print(data);
+		print(data._maps)
+		print(data._maps[HTerrainData.CHANNEL_COLOR][0].image)
+		var colormap : Image = data.get_image(HTerrainData.CHANNEL_COLOR)
+		print(colormap)
+
+	# Modify the image
+		#var position = Vector2(42, 36)
+		for x in range(30, 230):
+			for y in range(30, 230):
+				#colormap.set_pixel(x, y, Color(1, 0, 0))
+				pass
+	# Notify the terrain of our change
+		#data.notify_region_changed(Rect2(position.x, position.y, 1, 1), HTerrainData.CHANNEL_COLOR)
