8e37690e4ef6477dbdaf6b919abe55e3b49261ac
cbbor <c.b.borisova@gmail.com>
2023-12-07 19:50:49 -0600

    spawned terrain successfully

--- scripts/initialize.gd
+++ scripts/initialize.gd
@@ -1,9 +1,17 @@
 extends Node3D
 
+const HTerrain = preload("res://addons/zylann.hterrain/hterrain.gd")
 const HTerrainData = preload("res://addons/zylann.hterrain/hterrain_data.gd")
-@onready var _terrain = %Terrain
-var xr_interface: XRInterface
+const HTerrainTextureSet = preload("res://addons/zylann.hterrain/hterrain_texture_set.gd")
 
+# You may want to change paths to your own textures
+#var grass_texture = load("res://addons/zylann.hterrain_demo/textures/ground/grass_albedo_bump.png")
+#var sand_texture = load("res://addons/zylann.hterrain_demo/textures/ground/sand_albedo_bump.png")
+#var leaves_texture = load("res://addons/zylann.hterrain_demo/textures/ground/leaves_albedo_bump.png")
+#@onready var _terrain = %Terrain
+var terrain_data = null
+var xr_interface: XRInterface
+var terrain = null
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -17,22 +25,86 @@ func _ready():
 		get_viewport().use_xr = true
 	else:
 		print("OpenXR not initialized. Please check if your headset is connected.")
+	terrain_data = HTerrainData.new()
+	terrain_data.resize(513)
+	
+	
+	var noise = FastNoiseLite.new()
+	var noise_multiplier = 50.0
 
-func _on_button_pressed(name):
-	if (name == 'trigger_click'):
-		# Get the image
-		var data : HTerrainData = _terrain.get_data()
-		print(data);
-		print(data._maps)
-		print(data._maps[HTerrainData.CHANNEL_COLOR][0].image)
-		var colormap : Image = data.get_image(HTerrainData.CHANNEL_COLOR)
-		print(colormap)
+	# Get access to terrain maps
+	var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
+	var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
+	var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
 
-	# Modify the image
-		#var position = Vector2(42, 36)
-		for x in range(30, 230):
-			for y in range(30, 230):
-				#colormap.set_pixel(x, y, Color(1, 0, 0))
-				pass
-	# Notify the terrain of our change
-		#data.notify_region_changed(Rect2(position.x, position.y, 1, 1), HTerrainData.CHANNEL_COLOR)
+	# Generate terrain maps
+	# Note: this is an example with some arbitrary formulas,
+	# you may want to come up with your owns
+	for z in heightmap.get_height():
+		for x in heightmap.get_width():
+			# Generate height
+			var h = noise_multiplier * noise.get_noise_2d(x, z)
+
+			# Getting normal by generating extra heights directly from noise,
+			# so map borders won't have seams in case you stitch them
+			var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
+			var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
+			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
+
+			# Generate texture amounts
+			var splat = splatmap.get_pixel(x, z)
+			var slope = 4.0 * normal.dot(Vector3.UP) - 2.0
+			# Sand on the slopes
+			var sand_amount = clamp(1.0 - slope, 0.0, 1.0)
+			# Leaves below sea level
+			var leaves_amount = clamp(0.0 - h, 0.0, 1.0)
+			splat = splat.lerp(Color(0,1,0,0), sand_amount)
+			splat = splat.lerp(Color(0,0,1,0), leaves_amount)
+
+			heightmap.set_pixel(x, z, Color(h, 0, 0))
+			normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
+			splatmap.set_pixel(x, z, splat)
+
+	# Commit modifications so they get uploaded to the graphics card
+	var modified_region = Rect2(Vector2(), heightmap.get_size())
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
+	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
+
+	# Create texture set
+	# NOTE: usually this is not made from script, it can be built with editor tools
+#	var texture_set = HTerrainTextureSet.new()
+#	texture_set.set_mode(HTerrainTextureSet.MODE_TEXTURES)
+#	texture_set.insert_slot(-1)
+#	texture_set.set_texture(0, HTerrainTextureSet.TYPE_ALBEDO_BUMP, grass_texture)
+#	texture_set.insert_slot(-1)
+#	texture_set.set_texture(1, HTerrainTextureSet.TYPE_ALBEDO_BUMP, sand_texture)
+#	texture_set.insert_slot(-1)
+#	texture_set.set_texture(2, HTerrainTextureSet.TYPE_ALBEDO_BUMP, leaves_texture)
+
+	# Create terrain node
+	terrain = HTerrain.new()
+	terrain.set_shader_type(HTerrain.SHADER_CLASSIC4_LITE)
+	terrain.set_data(terrain_data)
+#	terrain.set_texture_set(texture_set)
+	terrain.position = Vector3(-200, -20, -200)
+	add_child(terrain)
+
+	# No need to call this, but you may need to if you edit the terrain later on
+	#terrain.update_collider()
+
+##func _on_button_pressed(name):
+##	if (name == 'trigger_click'):
+##		# Get the image
+##		var data : HTerrainData = _terrain.get_data()
+##		var colormap : Image = terrain_data.get_image(HTerrainData.CHANNEL_COLOR)
+##
+##	# Modify the image
+##		#var position = Vector2(42, 36)
+##		for x in range(-20, 20):
+##			for y in range(-20, 20):
+##				colormap.set_pixel(x, y, Color(1, 0, 0))
+##				terrain_data.notify_region_change(Rect2(x, y, 1, 1), HTerrainData.CHANNEL_COLOR)
+##		print("im done")
+#	# Notify the terrain of our change
+		
