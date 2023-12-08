1708b6b8f1838d6ae057df21f70966c411363e5c
cbbor <c.b.borisova@gmail.com>
2023-12-07 21:47:02 -0600

    apply terrain on trigger click

--- scripts/initialize.gd
+++ scripts/initialize.gd
@@ -12,6 +12,7 @@ const HTerrainTextureSet = preload("res://addons/zylann.hterrain/hterrain_textur
 var terrain_data = null
 var xr_interface: XRInterface
 var terrain = null
+var is_sin = true
 
 func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
@@ -27,49 +28,6 @@ func _ready():
 		print("OpenXR not initialized. Please check if your headset is connected.")
 	terrain_data = HTerrainData.new()
 	terrain_data.resize(513)
-	
-	
-	var noise = FastNoiseLite.new()
-	var noise_multiplier = 50.0
-
-	# Get access to terrain maps
-	var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
-	var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
-	var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
-
-	# Generate terrain maps
-	# Note: this is an example with some arbitrary formulas,
-	# you may want to come up with your owns
-	for z in heightmap.get_height():
-		for x in heightmap.get_width():
-			# Generate height
-			var h = noise_multiplier * noise.get_noise_2d(x, z)
-
-			# Getting normal by generating extra heights directly from noise,
-			# so map borders won't have seams in case you stitch them
-			var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
-			var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
-			var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
-
-			# Generate texture amounts
-			var splat = splatmap.get_pixel(x, z)
-			var slope = 4.0 * normal.dot(Vector3.UP) - 2.0
-			# Sand on the slopes
-			var sand_amount = clamp(1.0 - slope, 0.0, 1.0)
-			# Leaves below sea level
-			var leaves_amount = clamp(0.0 - h, 0.0, 1.0)
-			splat = splat.lerp(Color(0,1,0,0), sand_amount)
-			splat = splat.lerp(Color(0,0,1,0), leaves_amount)
-
-			heightmap.set_pixel(x, z, Color(h, 0, 0))
-			normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
-			splatmap.set_pixel(x, z, splat)
-
-	# Commit modifications so they get uploaded to the graphics card
-	var modified_region = Rect2(Vector2(), heightmap.get_size())
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
-	terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
 
 	# Create texture set
 	# NOTE: usually this is not made from script, it can be built with editor tools
@@ -93,18 +51,49 @@ func _ready():
 	# No need to call this, but you may need to if you edit the terrain later on
 	#terrain.update_collider()
 
-##func _on_button_pressed(name):
-##	if (name == 'trigger_click'):
-##		# Get the image
-##		var data : HTerrainData = _terrain.get_data()
-##		var colormap : Image = terrain_data.get_image(HTerrainData.CHANNEL_COLOR)
-##
-##	# Modify the image
-##		#var position = Vector2(42, 36)
-##		for x in range(-20, 20):
-##			for y in range(-20, 20):
-##				colormap.set_pixel(x, y, Color(1, 0, 0))
-##				terrain_data.notify_region_change(Rect2(x, y, 1, 1), HTerrainData.CHANNEL_COLOR)
-##		print("im done")
-#	# Notify the terrain of our change
-		
+func _on_button_pressed(name):
+	if (name == 'trigger_click'):
+		if (is_sin):
+			var noise = FastNoiseLite.new()
+			var noise_multiplier = 50.0
+			# Get access to terrain maps
+			var heightmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_HEIGHT)
+			var normalmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_NORMAL)
+			var splatmap: Image = terrain_data.get_image(HTerrainData.CHANNEL_SPLAT)
+			var colormap: Image = terrain_data.get_image(HTerrainData.CHANNEL_COLOR)
+
+			# Generate terrain maps
+			# Note: this is an example with some arbitrary formulas,
+			# you may want to come up with your owns
+			for z in heightmap.get_height():
+				for x in heightmap.get_width():
+					# Generate height
+					var h = noise_multiplier * noise.get_noise_2d(x, z)
+
+					# Getting normal by generating extra heights directly from noise,
+					# so map borders won't have seams in case you stitch them
+					var h_right = noise_multiplier * noise.get_noise_2d(x + 0.1, z)
+					var h_forward = noise_multiplier * noise.get_noise_2d(x, z + 0.1)
+					var normal = Vector3(h - h_right, 0.1, h_forward - h).normalized()
+
+					# Generate texture amounts
+					var splat = splatmap.get_pixel(x, z)
+					var slope = 4.0 * normal.dot(Vector3.UP) - 2.0
+					# Sand on the slopes
+					var sand_amount = clamp(1.0 - slope, 0.0, 1.0)
+					# Leaves below sea level
+					var leaves_amount = clamp(0.0 - h, 0.0, 1.0)
+					splat = splat.lerp(Color(0,1,0,0), sand_amount)
+					splat = splat.lerp(Color(0,0,1,0), leaves_amount)
+
+					heightmap.set_pixel(x, z, Color(h, 0, 0))
+					normalmap.set_pixel(x, z, HTerrainData.encode_normal(normal))
+					splatmap.set_pixel(x, z, splat)
+					colormap.set_pixel(x, z, Color(1,0, 0))
+
+			# Commit modifications so they get uploaded to the graphics card
+			var modified_region = Rect2(Vector2(), heightmap.get_size())
+			terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_HEIGHT)
+			terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_NORMAL)
+			terrain_data.notify_region_change(modified_region, HTerrainData.CHANNEL_SPLAT)
+
