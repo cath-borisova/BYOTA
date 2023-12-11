96346f5909e7cb793d491c1754114a2dcc164de4
wackyvoid <ruth36m@gmail.com>
2023-12-10 21:28:51 -0600

    new branch

--- scripts/grabber.gd
+++ scripts/grabber.gd
@@ -2,38 +2,36 @@ extends Node3D
 
 var grabbed_object: RigidBody3D = null
 var previous_transform: Transform3D
-var released = false
-var released_object = null
-var map_collision = null
+
+var target_left
+var target_right
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	map_collision = get_node("../../%MapContainer/Map/Collision")
+	target_left = %LeftController
+	target_right = %RightController
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 
 	# Copy the grabber's relative movement since the last frame to to the grabbed object
 	if self.grabbed_object:
-		self.grabbed_object.transform = self.transform * self.previous_transform.inverse() * self.grabbed_object.transform
-
-	self.previous_transform = self.transform
-#	print(map_collision)
-#	if released && map_collision.overlaps_body(released_object):
-#		map_collision.add_child(released_object)
-#		released = false
-#		released_object = null
-#	elif released && released_object.y == 0:
-#		released_object.scale = Vector3(10, 10, 10)
-#		released = false
-#		released_object = null
+		self.grabbed_object.transform = self.transform * self.previous_transform.affine_inverse() * self.grabbed_object.transform
+		self.grabbed_object.scale.x = target_left.global_position.distance_to(target_right.global_position)
+		self.grabbed_object.scale.y = target_left.global_position.distance_to(target_right.global_position)
+		self.grabbed_object.scale.z = target_left.global_position.distance_to(target_right.global_position)
 	
+	self.previous_transform = self.transform
+	
+
 func _on_button_pressed(button_name: String) -> void:
 	print("button pressed: " + button_name)
-
+	
 	# Stop if we have not clicked the grip button or we already are grabbing an object
 	if button_name != "grip_click" || self.grabbed_object != null:
 		return
-
+	
 	var grabbables = get_tree().get_nodes_in_group("grabbable")
 	var collision_area = $Area3D as Area3D
 
@@ -44,6 +42,7 @@ func _on_button_pressed(button_name: String) -> void:
 
 		# Check to see if the grabber and grabbable collision shapes are intersecting
 		if collision_area.overlaps_body(grabbable_body):
+	
 			# If the object is already grabbed by another grabber, release it first
 			var globals = get_node("/root/Globals")
 			for grabber in globals.active_grabbers:
@@ -54,10 +53,9 @@ func _on_button_pressed(button_name: String) -> void:
 
 			# Freeze the object physics and then grab it
 			grabbable_body.freeze = true
-			grabbable_body.scale = Vector3(0.1, 0.1, 0.1)
 			self.grabbed_object = grabbable_body
 			globals.active_grabbers.push_back(self)
-
+	
 func _on_button_released(button_name: String) -> void:
 	print("button released: " + button_name)
 	
@@ -66,12 +64,11 @@ func _on_button_released(button_name: String) -> void:
 		return
 
 	# Release the grabbed object and unfreeze it
-	released = true
 	self.grabbed_object.freeze = false
 	self.grabbed_object.linear_velocity = Vector3(0, -0.1, 0)
 	self.grabbed_object.angular_velocity = Vector3.ZERO
-	released_object = self.grabbed_object
 	self.grabbed_object = null
+
 	# Remove this grabber from the array of active grabbers
 	var globals = get_node("/root/Globals")
 	globals.active_grabbers.remove_at(globals.active_grabbers.find(self))
