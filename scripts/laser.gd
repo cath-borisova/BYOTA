fad5b8fbd87765900ea9691038488b961f5fb251
Katya Borisova <c.b.borisova@gmail.com>
2024-01-18 15:55:46 -0600

    fix laser randomly appearing

--- scripts/laser.gd
+++ scripts/laser.gd
@@ -4,27 +4,28 @@ func _ready():
 	self.visible = false
 
 func _process(delta):
-	self.scale.z = 100
-	self.position.z = -50
+	if %GraphRigidBody.visible
+		self.scale.z = 100
+		self.position.z = -50
 
-	var start_point = get_parent().global_position
-	var end_point = get_parent().global_transform * Vector3(0, 0, -100)
+		var start_point = get_parent().global_position
+		var end_point = get_parent().global_transform * Vector3(0, 0, -100)
 
-	var query = PhysicsRayQueryParameters3D.create(start_point, end_point)
-	query.collide_with_areas = true
+		var query = PhysicsRayQueryParameters3D.create(start_point, end_point)
+		query.collide_with_areas = true
 
-	var result = get_world_3d().direct_space_state.intersect_ray(query)
+		var result = get_world_3d().direct_space_state.intersect_ray(query)
 
-	var is_visible = false
-	if !result.is_empty():
-		var collision_distance = result["position"].distance_to(start_point)
-		self.scale.z = collision_distance
-		self.position.z = -collision_distance / 2
-		var buttons = get_tree().get_nodes_in_group("button")
-		for button in buttons:
-			if result["collider_id"] == button.get_instance_id():
-				is_visible = true
-		self.visible = is_visible
+		var is_visible = false
+		if !result.is_empty():
+			var collision_distance = result["position"].distance_to(start_point)
+			self.scale.z = collision_distance
+			self.position.z = -collision_distance / 2
+			var buttons = get_tree().get_nodes_in_group("button")
+			for button in buttons:
+				if result["collider_id"] == button.get_instance_id():
+					is_visible = true
+			self.visible = is_visible
 
 func _on_controller_button_pressed(button_name):
 	if button_name == "trigger_click" && %GraphRigidBody.visible:
