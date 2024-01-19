c489225e8b1a5bcaff444c29210e14aed840a958
wackyvoid <ruth36m@gmail.com>
2024-01-19 13:02:05 -0600

    finished equation display text

--- scripts/miniobject.gd
+++ scripts/miniobject.gd
@@ -47,7 +47,11 @@ func _process(_delta):
 				new_shape.global_position.y = big_height / 5.13 - 0.2
 			else:
 				new_shape.global_position = big_position
-				
+			# y = equations[x][z][0] * sin(equations[x][z][1] * x) * cos(equations[x][z][2] * z)
+			var equation = globals.get_equation((big_position.x+50)*5.13,(big_position.z+50)*5.13)
+			print("y = ", equation[0], " * sin(",equation[1]," * ", new_shape.global_position.x, ") * cos(", equation[2], " * ", new_shape.global_position.z, ")") 
+			get_node("/root/Main/"+new_shape.name+"/equation").text = "y = "+ str(equation[0]) + " * sin(" + str(equation[1]) + " * " + str(new_shape.global_position.x) + ") * cos("+ str(equation[2])+ " * "+ str(new_shape.global_position.z) + ")"
+			#.get_child("equation").text = "y = "+ str(equation[0]) + " * sin(" + str(equation[1]) + " * " + str(new_shape.global_position.x) + ") * cos("+ str(equation[2])+ " * "+ str(new_shape.global_position.z) + ")"
 		elif area3d.overlaps_body(ground):
 			self.queue_free()
 		
